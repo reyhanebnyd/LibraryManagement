2c405abb3f35a189aa4ec73ef768367f4556014f
reyhanebnyd <bonyadiho@gmail.com>
2024-09-02 01:49:45 +0330

    model change

--- LibraryManagement/models.py
+++ LibraryManagement/models.py
@@ -16,11 +16,4 @@ class Book(models.Model):
         return self.title
 
 
-# class BookFilter(models.Model):  
-#     book = models.ForeignKey(Book, on_delete=models.CASCADE)  
-#     filter_date = models.DateField(null=True, blank=True)  
-#     filter_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)  
-
-#     def __str__(self):  
-#         return f"Filter for {self.book.title}"
 
